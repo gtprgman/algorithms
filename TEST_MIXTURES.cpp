7acdca7b208192bb457adaead3e6e94e60f25496
gtprgman <eddywu32@gmail.com>
2022-07-30 12:50:21 +0700

    Revised Reflected to the updated ' mixutil.h '.

--- TEST_MIXTURES.cpp
+++ TEST_MIXTURES.cpp
@@ -1,24 +1,25 @@
 #include "mixutil.h"
 
 
-
-
-
 int main()
 {
 	Alloc_Share<int> baseShr = SHARED_CREATE<int>(3);
-	SHARED_ARRAY_TYPE<int> pshr = baseShr(3); // calls the functor
+	SHARED_ARRAY<int> pshr = baseShr(3); // calls the functor
 
-	SHARED_INIT<3,int>(pshr, 0, 100, 200, 300);
+	std::initializer_list<int> lsn{ 150,250,350 };
+
+	SHARED_INIT<int,3>(pshr, 0, 100, 200, 300);
+
+	smart_print(&pshr[0], &pshr[3]);
 	
+	SHARED_INIT<int>(pshr, lsn);
 
-	FOR_EACH(&pshr[0], &pshr[3], [](auto const& ptr)->decltype(void()) {
-		std::cout << ptr << ", " << std::endl;
-	}
-	);
-	
+	std::cout << "\n";
 
+
+	smart_print(&pshr[0], &pshr[3]);
 
 	system("PAUSE");
 	return 0;
 }
+
