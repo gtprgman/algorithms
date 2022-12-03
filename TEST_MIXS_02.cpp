fcc67594680c2b6b0e0a485e2e3d66c2fe9af5d1
gtprgman <eddywu32@gmail.com>
2022-12-03 11:14:23 +0700

    RECENT UPDATED (12/03/2022)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -1,65 +1,44 @@
-
 #include "mixutil.h"
-
-
-using namespace mix;
-using namespace mix::ptr_type;
-using namespace mix::smart_ptr;
-using namespace mix::data;
+#include "mixer.h"
 
 
 
-// custom deleter for single shared pointer
-template < class TP >
-struct del_shared
-{
-	bool operator()(TP* _tp) {
-		std::cout << "user custom deleter called .. " << std::endl;
-		static_assert(sizeof(_tp) > 0, "invalid pointer argument for '_tp' ");
-		delete _tp;
-		return (nullptr == _tp);
-	}
-};
+
+int main() {
+
+	
+	// codes demo for unique pointers.
+	unique_array_ptr<Bucket> ubFactory;
+
+	UNIQUE_ARRAY<Bucket> upb = ubFactory.create(2);
+
+	upb = ubFactory.initialize({ Bucket("u1"), Bucket("u2") });
+
+	iPrinter<Bucket*>::_print_(upb.get(), upb.get() + 2);
+
+	iList<Bucket> bList{ Bucket("b1"), Bucket("b2")};
+
+	upb = ubFactory.initialize(bList);
+
+	iPrinter<Bucket*>::_print_(upb.get(), upb.get() + 2);
 
 
-
-int main(void)
-{
-	// uses default deleter
-	shareP<Bucket> spb = sp_create<Bucket>();
-
-	std::cout << "a new share bucket just created on the heap \n";
-	std::cout << "data for the new shared bucket: " << spb->data() << "\n\n";
-
-	*spb = Bucket("b1");
-
-	std::cout << "a copy of Bucket object 'b1' is stored \n";
-	std::cout << "on the created shared space owned by 'spb '.\n";
-	std::cout << "data for the shared bucket: " << spb->data() << "\n\n";
-
-	// added custom deleter
-	shareP<Bucket> spb2 = sp_create<Bucket, del_shared<Bucket> >(2);
-
-	std::cout << "a new share bucket is created on the heap \n";
-	std::cout << "data for the new shared bucket: " << spb2->data() << "\n\n";
-
-	*spb2 = Bucket("b2");
-
-	std::cout << "a copy of a new created Bucket 'b2 ' \n";
-	std::cout << "just stored on the shared space owned by 'spb2' \n";
-	std::cout << "data for 'spb2' : " << spb2->data() << "\n\n";
-
-
+	// codes demo for shared pointers.
 
 	std::cout << "\n\n";
-	
+
+	Alloc_Share<Bucket> shrFactory(2);
+
+	SHARED_ARRAY<Bucket> spb = shrFactory.get_shared();
+
+	iPrinter<Bucket*>::_print_(spb.get(), spb.get() + 2);
+
+	spb = shrFactory.initialize(bList);
+
+	iPrinter<Bucket*>::_print_(spb.get(), spb.get() + 2);
+
+
+
 	system("PAUSE");
 	return 0;
-	
-	
 }
-
-
-
-
-
