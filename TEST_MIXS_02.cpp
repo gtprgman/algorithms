89c2d63c648c7031b25008424ad5151ca4994b42
gtprgman <eddywu32@gmail.com>
2023-05-13 08:24:41 +0700

    TESTING UPDATED BNode(2023/May/13)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -6,26 +6,48 @@ using P_NODE = BNode*;
 
 
 int main() {
-	
 	P_NODE nd0 = ALLOC_N("Tom");
 
-	if P_ASSERT(nd0) nd0->Print();
+	nd0->Add(ALLOC_N("Jade"));
+	nd0->Add(ALLOC_N("Kaleen"));
 
-	
-	P_NODE nd1 = treeAdd(nd0, "Peter");
+	nd0->Print(); std::cout << "\n\n";
 
-	std::cout << "\n";
+	P_NODE nd1 = nd0->Left();
+
+	nd1->Print(); std::cout << "\n\n";
+
+
+	nd0->Add(ALLOC_N("Ulfha"));
+
+	nd0->Print(); std::cout << "\n\n";
+
+
+	P_NODE nd2 = traverseNodes(nd0, "Jade");
+
+	nd2->Print();
+
+	std::cout << "\n\n";
+
+	P_NODE nd3 = treeAdd(nd0, "Heather");
+
+	nd3->Print();
+
+	std::cout << "\n\n";
 
 	nd0->Print();
 
+	std::cout << "\n\n";
 
-	std::cout << "\n";
+	nd0->Add(ALLOC_N("Alice"));
 
-	nd1->Print();
-
+	nd0->Left()->Print();
 
 	std::cout << "\n\n";
-	
+
+	nd0->Left()->Left()->Print();
+
+	std::cout << "\n\n";	
 }
 
 
