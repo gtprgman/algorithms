dc704918813c77265d0da63a9dee388d42bad845
gtprgman <eddywu32@gmail.com>
2023-05-18 12:14:09 +0700

    TESTING NEW UPDATED (2023/May/18)
    Testing Insert() method...

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -1,3 +1,4 @@
+
 #include "mixutil.h"
 #include "mixer.h"
 
@@ -5,49 +6,49 @@
 using P_NODE = BNode*;
 
 
-int main() {
+int main()
+{
 	P_NODE nd0 = ALLOC_N("Tom");
 
+
 	nd0->Add(ALLOC_N("Jade"));
-	nd0->Add(ALLOC_N("Kaleen"));
-
-	nd0->Print(); std::cout << "\n\n";
-
-	P_NODE nd1 = nd0->Left();
-
-	nd1->Print(); std::cout << "\n\n";
-
-
 	nd0->Add(ALLOC_N("Ulfha"));
 
 	nd0->Print(); std::cout << "\n\n";
 
+	P_NODE nd1 = nd0->Left(); // jade
 
-	P_NODE nd2 = traverseNodes(nd0, "Jade");
-
-	nd2->Print();
-
-	std::cout << "\n\n";
-
-	P_NODE nd3 = treeAdd(nd0, "Heather");
-
-	nd3->Print();
-
-	std::cout << "\n\n";
+	nd1->Insert(ALLOC_N("Irene"));
 
 	nd0->Print();
 
 	std::cout << "\n\n";
 
-	nd0->Add(ALLOC_N("Alice"));
-
-	nd0->Left()->Print();
+	std::cout << "Left Height: " << BNode::T_LEFT() << "\n";
+	std::cout << "Right Height: " << BNode::T_RIGHT() << "\n";
 
 	std::cout << "\n\n";
 
-	nd0->Left()->Left()->Print();
+	nd1->Print();
 
-	std::cout << "\n\n";	
+	P_NODE nd01 = nd1->Parent();
+
+	std::cout << "\n\n";
+
+	nd01->Print();
+
+	std::cout << "\n\n";
+
+	nd01 = treeAdd(nd0, "Gerry");
+
+	nd01->Print();
+
+	std::cout << "\n\n";
+
+	std::cout << "Left Height: " << BNode::T_LEFT() << "\n";
+	std::cout << "Right Height: " << BNode::T_RIGHT() << "\n";
+
+	std::cout << "\n\n";
+
 }
-
 
