4a39465cc6f10d1a5188460f1c8d735537b8a085
gtprgman <eddywu32@gmail.com>
2023-06-07 10:12:24 +0700

    RECENT UPDATED (2023/JUNE/07)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -8,28 +8,53 @@ using P_NODE = BNode*;
 int main()
 {
 	P_NODE nRoot = ALLOC_N(15);
-	P_NODE nTemp = nullptr;
+	P_NODE n1 = nullptr, n2 = nullptr;
 	
-	setTopRoot(nRoot); // this is a 'must' for BNode::Find()
+	setTopRoot(nRoot);
 
 	nRoot->Add(ALLOC_N(9));
 	nRoot->Add(ALLOC_N(23));
 	nRoot->Print();
 
-	BNode::Find(23)->Print();
-
 	nRoot->Add(ALLOC_N(17));
 	nRoot->Add(ALLOC_N(28));
 
-	BNode::Find(18)->Print();
-
-	nRoot->Right()->Print();
-
 	nRoot->Add(ALLOC_N(3));
 	nRoot->Add(ALLOC_N(12));
-
-	nRoot->Left()->Print();
 	
-	NULL2P(nTemp, nRoot);
+	nRoot->Add(ALLOC_N(4));
+	nRoot->Add(ALLOC_N(8));
+
+	BNode::Find(9)->Print();
+
+	std::cout << "Node 28 has childs? :" << HAS_CHILDS(BNode::Find(28)) << "\n\n";
+	std::cout << "Node 4 has childs? :" << HAS_CHILDS(BNode::Find(4)) << "\n\n";
+
+	TALL;
+	RET;
+
+	std::cout << "Removing Node 3.. " << "\n\n";
+
+	BNode::Find(3)->Remove();
+	BNode::Find(9)->Print();
+
+
+	TALL;
+	RET;
+
+	BNode::Find(4)->Print();
+
+	BNode::Find(23)->Print();
+
+	std::cout << "Removing Node 28.. " << "\n\n";
+
+	BNode::Find(28)->Remove();
+	BNode::Find(23)->Print();
+
+
+	TALL;
+	RET;
+
+	NULL3P(n1,n2, nRoot);
 	
 }
