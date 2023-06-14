98832cea77b4ef60ab91e7f6e35855c0015d038e
gtprgman <eddywu32@gmail.com>
2023-06-14 10:54:53 +0700

    RECENT UPDATED (2023/JUNE/14)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -2,59 +2,64 @@
 #include <string>
 #include "mixtree.h"
 
-using P_NODE = BNode*;
+using PNODE = BNode*;
 
 
 int main()
 {
-	P_NODE nRoot = ALLOC_N(15);
-	P_NODE n1 = nullptr, n2 = nullptr;
-	
+	PNODE nRoot = ALLOC_N(15);
 	setTopRoot(nRoot);
+
 
 	nRoot->Add(ALLOC_N(9));
 	nRoot->Add(ALLOC_N(23));
-	nRoot->Print();
 
 	nRoot->Add(ALLOC_N(17));
 	nRoot->Add(ALLOC_N(28));
 
-	nRoot->Add(ALLOC_N(3));
-	nRoot->Add(ALLOC_N(12));
-	
 	nRoot->Add(ALLOC_N(4));
+	nRoot->Add(ALLOC_N(12));
+
+	nRoot->Add(ALLOC_N(3));
 	nRoot->Add(ALLOC_N(8));
 
-	BNode::Find(9)->Print();
+	Find(3)->Print();
+	Find(3)->Parent()->Print(); // node 4->Print();
 
-	std::cout << "Node 28 has childs? :" << HAS_CHILDS(BNode::Find(28)) << "\n\n";
-	std::cout << "Node 4 has childs? :" << HAS_CHILDS(BNode::Find(4)) << "\n\n";
+	Find(9)->Print();
 
-	TALL;
-	RET;
+	
+	// recall that BNode::Remove() is now implied move instead of copy..
+	std::cout << "Removing Node 4.." << "\n\n";
+	Find(4)->Remove()->Print();;
 
-	std::cout << "Removing Node 3.. " << "\n\n";
+	setTopRoot(nRoot);
 
-	BNode::Find(3)->Remove();
-	BNode::Find(9)->Print();
+	// to hinder being bewildered by the extracted error statement of Find()
+	PNODE nx = Find(4);
 
+	nx->Print();
 
-	TALL;
-	RET;
+	nx = Find(9);
 
-	BNode::Find(4)->Print();
+	nRoot->Print();
 
-	BNode::Find(23)->Print();
+	nx->Print();
+
+	nx = Find(28)->Parent();
+
+	nx->Print();
+
+	Find(28)->Print();
 
 	std::cout << "Removing Node 28.. " << "\n\n";
 
-	BNode::Find(28)->Remove();
-	BNode::Find(23)->Print();
+	Find(28)->Remove()->Print();
+	setTopRoot(nRoot);
 
+	nx->Print();
 
-	TALL;
-	RET;
+	NULL2P(nRoot,nx);
 
-	NULL3P(n1,n2, nRoot);
-	
+	return 0;
 }
