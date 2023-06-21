c5b4d1d47011cbd1b012dd09091f39c77f4f9b14
gtprgman <eddywu32@gmail.com>
2023-06-21 10:59:06 +0700

    RECENT UPDATED (2023/JUNE/21)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -7,59 +7,44 @@ using PNODE = BNode*;
 
 int main()
 {
-	PNODE nRoot = ALLOC_N(15);
-	nRoot->setTopRoot(nRoot);
+	T_INFO nfo;
 
+	PNODE nRoot = ALLOC_N(15);
+	nRoot->setTopRoot();
 
 	nRoot->Add(ALLOC_N(9));
-	nRoot->Add(ALLOC_N(23));
+	nRoot->Print();
 
+
+	nRoot->leftTop()->Print();
+	nRoot->rightTop()->Print();
+
+	L_HEIGHT(nRoot);
+	R_HEIGHT(nRoot);
+
+	LEFT_END_INFO(nRoot);
+	RIGHT_END_INFO(nRoot);
+
+	
+	nRoot->Add(ALLOC_N(23));
 	nRoot->Add(ALLOC_N(17));
 	nRoot->Add(ALLOC_N(28));
 
-	nRoot->Add(ALLOC_N(4));
 	nRoot->Add(ALLOC_N(12));
-
+	nRoot->Add(ALLOC_N(4));
 	nRoot->Add(ALLOC_N(3));
-	nRoot->Add(ALLOC_N(8));
+	nRoot->Add(ALLOC_N(8)); 
 
-	nRoot->Find(3)->Print();
-	nRoot->Find(3)->Parent()->Print(); // node 4->Print();
+	
+	L_HEIGHT(nRoot);
+	R_HEIGHT(nRoot);
 
-	nRoot->Find(9)->Print();
+	LEFT_END_INFO(nRoot);
+	RIGHT_END_INFO(nRoot);
 
-	// recall that BNode::Remove() is now implied move instead of copy..
-	std::cout << "Removing Node 4.." << "\n\n";
-	nRoot->Find(4)->Remove()->Print();;
+	nRoot->Find(4)->Print();
 
-	PNODE nx = nRoot->Find(4);
-
-	nx->Print();
-
-	nx = nRoot->Find(9);
-
-	nRoot->Print();
-
-	nx->Print();
-
-	nx = nRoot->Find(28)->Parent();
-
-	nx->Print();
-
-	nRoot->Find(28)->Print();
-
-	std::cout << "Removing Node 28.. " << "\n\n";
-
-	nRoot->Find(28)->Remove()->Print();
-
-	nx->Print();
-
-	nRoot->Remove()->Print();
-
-	RET;
-
-
-	NULL2P(nRoot,nx);
+	NULLP(nRoot);
 
 	return 0;
 }
