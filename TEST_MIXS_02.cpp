94cd3ccee3a46121e44aeadeee38f9ea38f5d39e
gtprgman <eddywu32@gmail.com>
2023-06-24 13:14:42 +0700

    RECENT UPDATED (2023/JUNE/24)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -7,45 +7,39 @@ using PNODE = BNode*;
 
 int main()
 {
-	T_INFO nfo;
-
 	PNODE nRoot = ALLOC_N(15);
 	nRoot->setTopRoot();
 
 	nRoot->Add(ALLOC_N(9));
-	nRoot->Print();
-
-
-	nRoot->leftTop()->Print();
-	nRoot->rightTop()->Print();
-
-	L_HEIGHT(nRoot);
-	R_HEIGHT(nRoot);
-
-	LEFT_END_INFO(nRoot);
-	RIGHT_END_INFO(nRoot);
-
-	
 	nRoot->Add(ALLOC_N(23));
+	nRoot->Add(ALLOC_N(4));
+	nRoot->Add(ALLOC_N(12));
+	nRoot->Add(ALLOC_N(8));
+	nRoot->Add(ALLOC_N(3));
 	nRoot->Add(ALLOC_N(17));
 	nRoot->Add(ALLOC_N(28));
-
-	nRoot->Add(ALLOC_N(12));
-	nRoot->Add(ALLOC_N(4));
-	nRoot->Add(ALLOC_N(3));
-	nRoot->Add(ALLOC_N(8)); 
-
+	
+	nRoot->Add(ALLOC_N(2));
+	nRoot->Add(ALLOC_N(11));
+	nRoot->Add(ALLOC_N(5));
 	
 	L_HEIGHT(nRoot);
 	R_HEIGHT(nRoot);
+	
+	nRoot->Find(2)->Print();
 
-	LEFT_END_INFO(nRoot);
-	RIGHT_END_INFO(nRoot);
+	nRoot->Find(11)->Print();
 
-	nRoot->Find(4)->Print();
+	nRoot->Find(28)->Print();
+
+	L_HEIGHT(nRoot);
+	R_HEIGHT(nRoot);
+
+	LEFT_TAIL(nRoot);
+	RIGHT_TAIL(nRoot);
+
 
 	NULLP(nRoot);
-
 	return 0;
+	
 }
-
