931fa35603b8e1fb79bae9138afebe31d2631ab9
gtprgman <eddywu32@gmail.com>
2023-07-29 11:10:00 +0700

    RECENT UPDATED (2023/JULY/29)

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 	PNODE nRoot = ALLOC_N(15);
-	setTopRoot(&nRoot);
+	BNode::setTopRoot(&nRoot); 
 
 	std::cout << "Adding up.. " << "\n\n";
 
@@ -16,16 +16,18 @@ int main()
 	nRoot->Add(ALLOC_N(12));
 	nRoot->Add(ALLOC_N(8));
 	nRoot->Add(ALLOC_N(3)); // R_TURNS happens after this one
-	
+
 	nRoot->Print();
+	// try find ,remove or modify any nodes to ascertain the R_TURNS()
+	nRoot->Find(23)->Print();
 
 	L_HEIGHT(nRoot);
 	R_HEIGHT(nRoot);
 	RET;
 
-
+	/*
 	nRoot->Add(ALLOC_N(17));
-	nRoot->Add(ALLOC_N(28));
+	nRoot->Add(ALLOC_N(28)); 
 
 	nRoot->Add(ALLOC_N(2));
 	nRoot->Add(ALLOC_N(11));
@@ -33,16 +35,14 @@ int main()
 
 	nRoot->Add(ALLOC_N(16));
 	nRoot->Add(ALLOC_N(35));
-	nRoot->Add(ALLOC_N(52));
+	nRoot->Add(ALLOC_N(52)); // L_TURNS() happens after this one
 
-	nRoot->Print();
+	*/
 
-	L_HEIGHT(nRoot);
-	R_HEIGHT(nRoot);
-	RET;
-
-
+	
 	delete nRoot;
+
+	return -1;	
 	
 }
 
