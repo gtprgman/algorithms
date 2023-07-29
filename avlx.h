73e52e8736c27d0518df27fb09428963d23c3109
gtprgman <eddywu32@gmail.com>
2023-07-29 11:10:58 +0700

    RECENT UPDATED (2023/JULY/29)

--- avlx.h
+++ avlx.h
@@ -1,4 +1,10 @@
 #pragma once
+
+
+static BNode* pRoot1 = nullptr;
+static BNode* pRoot2 = nullptr;
+
+
 struct AVL {
 
 	static UINT const LT_Count() { return LT; }
@@ -8,11 +14,12 @@ struct AVL {
 	static void set_RT_Count(const UINT nc) { RT = nc; }
 
 	static void reset_data() {
-		LT = 0; RT = 0; BAL = 0;
+		LT = 0; RT = 0; BAL = 0; _bTurnable = 0;
 	}
 
 	static void compute_balance() {
 		BAL = (LT > RT) ? LT - RT : RT - LT;
+		if (BAL > 1) _bTurnable = 1;
 	}
 
 	static void make_balance() {
@@ -25,13 +32,16 @@ struct AVL {
 		return BAL;
 	}
 
-	static BNode* newRight;
-	static BNode* newLeft;
+	static bool const allow_rotate() {
+		return _bTurnable;
+	}
+
 
 private:
 	static UINT LT; // maximum in the Left Branches
 	static UINT RT; // maximum in the Right Branches
 	static UINT BAL; // difference between LT & RT
+	static bool _bTurnable;
 
 	// rotate left branches to the right of the root.
 	static void R_TURNS();
@@ -46,7 +56,4 @@ private:
 unsigned int AVL::LT = 0;
 unsigned int AVL::RT = 0;
 unsigned int AVL::BAL = 0;
-
-BNode* AVL::newRight = nullptr;
-BNode* AVL::newLeft = nullptr;
-
+bool AVL::_bTurnable = 0;
