4a7495a4807279b774f0077070e31783c68991de
gtprgman <eddywu32@gmail.com>
2023-08-05 11:08:23 +0700

    COMPREHENSIVE TESTING ON R_TURNS() & L_TURNS()
    RECENT UPDATED (2023/AUG/05).

--- TEST_MIXS_02.cpp
+++ TEST_MIXS_02.cpp
@@ -18,14 +18,27 @@ int main()
 	nRoot->Add(ALLOC_N(3)); // R_TURNS happens after this one
 
 	nRoot->Print();
-	// try find ,remove or modify any nodes to ascertain the R_TURNS()
-	nRoot->Find(23)->Print();
+	// try to find ,remove or modify any nodes to ascertain the R_TURNS()
+	
+	
+	PRINT("Delete Node 4.")
+	DEL(nRoot, 4);
+	
+	NODE(nRoot, 4);
+	NODE(nRoot, 9);
+	NODE(nRoot, 3);
+
+	PRINT("Delete Node 23.")
+	DEL(nRoot, 23);
+	NODE(nRoot, 23);
+	NODE(nRoot, 15);
 
 	L_HEIGHT(nRoot);
 	R_HEIGHT(nRoot);
 	RET;
+	
+	//---------------------------------------------------------------
 
-	/*
 	nRoot->Add(ALLOC_N(17));
 	nRoot->Add(ALLOC_N(28)); 
 
@@ -34,15 +47,29 @@ int main()
 	nRoot->Add(ALLOC_N(5));
 
 	nRoot->Add(ALLOC_N(16));
-	nRoot->Add(ALLOC_N(35));
-	nRoot->Add(ALLOC_N(52)); // L_TURNS() happens after this one
+	nRoot->Add(ALLOC_N(35)); //L_TURNS() happens after this one
+	nRoot->Add(ALLOC_N(52)); 
 
-	*/
-
+	nRoot->Print();
+	// try to find, modify or remove any nodes to ascertain the L_TURNS()
 	
+	NODE(nRoot, 28);
+
+	PRINT("Delete Node 28.");
+	DEL(nRoot, 28);
+
+	NODE(nRoot, 28);
+	NODE(nRoot, 17);
+
+	NODE(nRoot, 35);
+
+	L_HEIGHT(nRoot);
+	R_HEIGHT(nRoot);
+
+
 	delete nRoot;
-
-	return -1;	
 	
+
+	return -1;
 }
 
