9ecf36b9c9c59e34ed18d2358c87faec51728dd8
gtprgman <eddywu32@gmail.com>
2023-09-16 10:59:23 +0700

    RECENT UPDATED (2023/09/16)

--- avl2x.h
+++ avl2x.h
@@ -5,67 +5,59 @@
 template <typename P>
 struct AVL {
 
-	using DType = typename NodeTraits<P>::memberType;
+	using DType = typename NodeTraits<std::remove_pointer_t<P>>::memberType;
 
-	static typename NodeTraits<P>::p_type pRoot1;
-	static typename NodeTraits<P>::p_type pRoot2;
+	typename NodeTraits<P>::p_type pRoot1 = nullptr;
+	typename NodeTraits<P>::p_type pRoot2 = nullptr;
 
-	static typename NodeTraits<P>::p_type newRoot1;
-	static typename NodeTraits<P>::p_type newRoot2;
+	typename NodeTraits<P>::p_type newRoot1 = nullptr;
+	typename NodeTraits<P>::p_type newRoot2 = nullptr;
 
-	constexpr UINT const LT_Count() { return LT; }
-	constexpr UINT const RT_Count() { return RT; }
+	static constexpr UINT const LT_Count() { return LT; }
+	static constexpr UINT const RT_Count() { return RT; }
 
-	constexpr void set_LT_Count(const UINT nc) { LT = nc; }
-	constexpr void set_RT_Count(const UINT nc) { RT = nc; }
+	static constexpr void set_LT_Count(const UINT nc) { LT = nc; }
+	static constexpr void set_RT_Count(const UINT nc) { RT = nc; }
 
-	constexpr void reset_data() {
+	static constexpr void reset_data() {
 		LT = 0; RT = 0; BAL = 0;
 	}
 
-	constexpr void compute_balance() {
-		BAL = (LT > RT) ? LT - RT : RT - LT;
+	static constexpr void compute_balance() {
+		BAL = (LT > RT)? LT - RT : RT - LT;
 	}
 
-	constexpr void make_balance() {
+	static constexpr void make_balance() {
 		if (LT > RT) R_TURNS();
 			else L_TURNS();
 	}
 
-	constexpr UINT const balance_value() {
+	static constexpr UINT const balance_value() {
 		return BAL;
 	}
 
 	
 private:
-	UINT LT; // maximum in the Left Branches
-	UINT RT; // maximum in the Right Branches
-	UINT BAL; // difference between LT & RT
+	static UINT LT; // maximum in the Left Branches
+	static UINT RT; // maximum in the Right Branches
+	static UINT BAL; // difference between LT & RT
 
 	// rotate left branches to the right of the root.
-	constexpr void R_TURNS();
+	static constexpr void R_TURNS();
 
 	// rotate right branches to the left of the root.
-	constexpr void L_TURNS();
+	static constexpr void L_TURNS();
 
 }; // END Of struct AVLX DEfs...
 
 
 // AVL<P> Members' Initialization..
 template <typename P>
-typename NodeTraits<P>::p_type AVL<P>::pRoot1 = nullptr;
+UINT AVL<P>::LT = 0;
 
 template <typename P>
-typename NodeTraits<P>::p_type AVL<P>::pRoot2 = nullptr;
+UINT AVL<P>::RT = 0;
 
 template <typename P>
-typename NodeTraits<P>::p_type AVL<P>::newRoot1 = nullptr;
-
-template <typename P>
-typename NodeTraits<P>::p_type AVL<P>::newRoot2 = nullptr;
-
-
-
-
-
+UINT AVL<P>::BAL = 0;
 
