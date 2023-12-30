56ca07f7a430d19423ef9e4d059f21ffe8014d46
gtprgman <eddywu32@gmail.com>
2023-12-30 12:22:13 +0700

    TEST HUFFMAN (Almost Ready)

--- TEST_HUFF_READY_03.cpp
+++ TEST_HUFF_READY_03.cpp
@@ -4,65 +4,45 @@
 
 
 int main() {
-	std::vector<node> freqNod, readyNods;
-
+	std::vector<node> nods,huffNods;
+	Byte rootValue = 0;
+	
 	const char s[] = "Ada Ate Apple";
+	const std::size_t LEN = strnlen_s(s, sizeof(s));
 
-	// LEN(s + '/0')
-	const std::size_t LEN = std::strlen(s);
-	Byte cnt = (Byte)total_values(s, LEN) / 2;
+	rootValue = (Byte)total_values(s, LEN);
 
-	node* data_t = (CONST_PTR)ALLOC_N<Byte>(cnt);
+	node* data_t = (CONST_PTR)ALLOC_N<Byte>(rootValue/2);
 	node::setRoot(ROOT2P(&data_t));
 	node::setSize(LEN);
 
-	// adding data to the tree
+	
+	// Plot data from any external source to the tree
 	for (const auto& c : s)
 		data_t->Add((Byte)c);
 
-	// collecting nodes from the tree
+
+	// Collect data from the tree
 	for (const auto& c : s) {
-		node nx = *PNODE<Byte>((Byte)c);
-		freqNod.emplace_back(TO_FREQ_NODE(node(nx)));
+		node ns = NODE_T(TO_FREQ_NODE(*PNODE<Byte>((Byte)c)) );
+		nods.emplace_back(ns);
+
 	}
 
-	// sorting the nodes
-	nodesSort<Byte>(freqNod, LEN);
+	sort_Nodes<double>(nods, LEN);
 
-	// filter the nodes to the separate container
-	for (const node& e : freqNod)
-		add_FrequencyNodes(readyNods, e);
-
-	/*
-	// Printing the ready nodes
-	for (const node& e : readyNods) {
-		RPRINT(e.dataValue()); RPRINT(e.FrequencyData());
-		RET;
-	}
-	*/
+	NPRINT(nods);
 	RET;
-	const std::size_t fLEN = readyNods.size();
-
-	std::unique_ptr<node, N_DELETER> ht = nullptr;
-	ht.reset( (CONST_PTR)huff_tree_create(readyNods, fLEN) );
-
-	node* f0 = ht.get();
-
-	std::vector<HF_REC> huff;
-
-	huffman_encode(huff, f0);
-
-
-	for (const HF_REC& _hf : huff)
-	{
-		RPRINT((char)_hf._data); RPRINT(_hf._bits);
-	}
-
 	RET;
 
+	frequency_Filter(huffNods, nods);
+
+	NPRINT(huffNods);
+
 	
-	
+	RET;
+	RET;
 	node::Dispose();
 
-	return -1;
+	return -1;	
 };
