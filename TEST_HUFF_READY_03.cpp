cf01f33fcd488cd41f05b5b203d1f27d8cbd0ea6
gtprgman <eddywu32@gmail.com>
2024-03-29 09:57:25 +0700

    TEST_HUFF_READY_03.cpp
    UPDATED:
    2024/MAR/29

--- TEST_HUFF_READY_03.cpp
+++ TEST_HUFF_READY_03.cpp
@@ -1,103 +1,60 @@
 #include <iostream>
-#include <map>
 #include "mixhuff.h"
 
 
-using BPAIR = std::pair<Bit, Byte>;
-
 
 int main() {
-	std::vector<HF_REC> hfc;
-	std::vector<node*> nvt;
-	std::vector<node> nods,huffNods;
-	Byte rootValue = 0;
-	
-	const char s[] = "Ada Ate Apple.";
+	std::string s = "Ada Ate Apple.";
+	std::vector<NODE_T> nods;
+	std::vector<node> bulk, refine;
 
-	const std::size_t LEN = strnlen_s(s, sizeof(s));
+	for (const auto& x : s)
+		nods.push_back(x);
 
-	const LongRange maxLen = (LongRange)(LEN - 1);
+	transForm(bulk, nods);
+	nods.clear();
 
-	rootValue = (Byte)total_values(s, LEN);
+	sort_Nodes<Byte>(bulk, bulk.size());
 
-	node* data_t = (CONST_PTR)ALLOC_N<Byte>(rootValue/2);
-	node::setRoot(ROOT2P(&data_t));
-	node::setSize(LEN);
+	filter_Nodes(refine, bulk);
+	bulk.clear();
 
-	
-	// Plot data from any external source to the tree
-	for (const auto& c : s)
-		data_t->Add((Byte)c);
-
-
-	// Collect data from the tree
-	for (const auto& c : s) {
-		//node ns = NODE_T(TO_FREQ_NODE(*PNODE<Byte>((Byte)c)) );
-		nods.emplace_back(ANODE(c));
-	}
-
-
-	if (nods.size() > 10)
-		merge_sort<double>(nods, (LongRange)nods.size());
-	else
-		range_sort<double>(nods, (LongRange)0, maxLen);
-
-	sort_Nodes<double>(nods, LEN-1);
-
-
-	filter_Nodes(huffNods, nods);
-
-	NPRINT(huffNods);
-
-	
-	RET;
-	RET;
-	
-	
-	std::unique_ptr<node> ht = nullptr;
-	ht.reset((CONST_PTR)huff_tree_create(nvt, huffNods, huffNods.size()));
-
-	node* fh = ht.get(); // get raw pointer
-
-
-	huffman_encode(hfc, fh);
-
-
-	for (const HF_REC& hf : hfc) {
-		RPRINT((char)hf._data); RPRINT(":"); RPRINT(hf.Bits()); RPRINT("|");
+	for (const auto& e : refine) {
+		RPRINT(e.FrequencyData()); RPRINT(" | ");
+		RPRINT(e.dataValue());
 		RET;
-	}
+	};
 
 	RET;
-	RET;
 
-	
-	std::map<Bit, Byte> mbp;
+	RPRINT("Filtered length: "); RPRINT(refine.size()); RET;
 
-	for (const HF_REC& hf : hfc) {
-		Bit x = 0b1, xc = 0b0;
+	range_sort<double>(refine, 0, refine.size()-1);
+	sort_Nodes<double>(refine, refine.size());
 
-		for (std::size_t j = 0; j < hf._bits.size(); j++) {
-			x &= (Bit)hf._bits[j];
-			x <<= (7 - j);
-			xc |= x;
-			x = 0b1;
-		}
-		mbp.emplace(std::pair<Bit, Byte>(xc, hf._data));
-	}
+	RPRINT("Sorted Length:"); RPRINT(refine.size()); RET2();
 
-
-	for (const BPAIR& cp : mbp) {
-		printf("% d ,", cp.first);
-		printf("% c ,", cp.second);
+	for (const auto& e : refine) {
+		RPRINT(e.FrequencyData()); RPRINT(" | ");
+		RPRINT(e.dataValue());
 		RET;
-	}
+	};
+	RET2();
+
+	huff_tree_create(refine, refine.size());
 
 
-	RET;
-	RET;
+	_TREE::_Root->Print();
 
+	LCOUNT(_TREE::_Root);
+	RCOUNT(_TREE::_Root);
 	
+	
+	refine.clear();
 	node::Dispose();
+	
+
+	RET2();
+
 	return -1;
-};
+}
