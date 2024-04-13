c53d5a9dca4c25ce548840590303b662f6304df5
gtprgman <eddywu32@gmail.com>
2024-04-13 10:57:55 +0700

    RECENT CHANGES
    2024/APR/13:
    Demonstrating the 'vector_search()' function.

--- TEST_HUFF_READY_03.cpp
+++ TEST_HUFF_READY_03.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
-#include "D:\MIXUTIL\User\Libs\mixhuff.h"
+#include "mixhuff.h"
+
+
 
 int main() {
+	std::string s = "She Sells Shells in the Sea Shore.";
+	std::vector<NODE_T> nodT;
+	std::vector<node> nods, ordered, treeNods;
+
+	for (const auto& x : s)
+		nodT.push_back(x);
+
+	transForm(nods, nodT); nodT.clear();
+
+	sort_Nodes<Byte>(nods,nods.size());
+
+	filter_Nodes(ordered, nods); nods.clear();
 	
-	node* ft = (CONST_PTR)ALLOC_N<double>(50.00);
+	node* ft = (CONST_PTR)ALLOC_N(50.00);
 	node::setRoot(ROOT2P(&ft));
-	node::setSize(3);
 
-	if (!vNods.empty()) vNods.clear();
+	for (auto& e : ordered)
+		ft->Add(std::forward<node>(e) );
 
-	ft->Add(NODE_T(65, 21.12));
-	ft->Add(NODE_T(112, 14.2857));
-	ft->Add(NODE_T(107, 65.0119));
+	transForm2(treeNods); ordered.clear();
+
+	//sort_Nodes<double>(treeNods, treeNods.size());
+
+	sort_Nodes<Byte>(treeNods, treeNods.size());
+
+	NPRINT(treeNods); RET;
+
+	NODE_T nf = 't';
+	node ndf;
 
 
-	for (const auto& e : vNods) {
-		RPRINT(e.dataValue()); RPRINT("|"); RPRINT(e.FrequencyData());
-		RET;
-	}
+	RPRINT("Found: ");  RPRINT(vector_search(treeNods, nf, ndf));
+	RPRINT((char)ndf());
+	
+	
 
 	RET2();
-	node::Dispose();
 
+	treeNods.clear();
+	node::Dispose();
 	return -1;
 }
