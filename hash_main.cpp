a4ec432956373475e65141a1f1c980783fee7810
gtprgman <eddywu32@gmail.com>
2024-05-04 10:30:43 +0700

    RECENT CHANGES
    2024/MAY/04

--- hash_main.cpp
+++ hash_main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
+#include "mixutil.h"
 #include "mixhuff.h"
 #include "mixhash.h"
 
 
 
 int main(int argc, const char* argv[]) {
-
-	cHash<NODE_T> hs(5);
-
-	hs = NODE_T(65);
-	hs = NODE_T(66);
-	hs = NODE_T(67);
-	hs = NODE_T(68);
-	hs = NODE_T(69);
-
-
-	hs = NODE_T(97);
-	hs = NODE_T(98);
-	hs = NODE_T(99);
-
-
-	for (std::size_t j = 0; j < 5; j++)
-		PRINT(hs[j].DataC());
-
+	cHash<cElem<NODE_T>> hs(10);
 	
+
+	for (int i = 0; i < 10; i++)
+		hs = cElem<NODE_T>(65 + i);
+
+
+	for (int j = 0; j < 10; j++)
+		PRINT((char)hs.elements(j).DataC());
+
+
 
 	RET2();
 
