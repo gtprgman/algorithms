185e1c139fe4f01eccf12c013c14906a15e6255e
gtprgman <eddywu32@gmail.com>
2024-05-11 10:22:07 +0700

    RECENT CHANGES
    2024/MAY/11

--- hash_main.cpp
+++ hash_main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+
 #include "mixutil.h"
 #include "mixhuff.h"
 #include "mixhash.h"
 
 
+ 
 
-int main(int argc, const char* argv[]) {
-	cHash<cElem<NODE_T>> hs(10);
-	
+int main(int argc, const char* argv[]) 
+{
+	cHash<cElem<NODE_T>> hs(5);
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 5; i++)
 		hs = cElem<NODE_T>(65 + i);
 
+	/* the following is adding a few number of extra elements
+	   to the cHash beyond the predetermined size in '_M'
+	*/
 
-	for (int j = 0; j < 10; j++)
-		PRINT((char)hs.elements(j).DataC());
+	hs = cElem<NODE_T>(70);
+	hs = cElem<NODE_T>(71);
+	hs = cElem<NODE_T>(72);
 
+	for (int j = 0; j < 5; j++)
+		PRINT(hs.elements(j).DataC());
 
+	RET;
+	
+	
+	PRINT(hs[70]._v);
+	PRINT(hs[71]._v);
+	PRINT(hs[72]._v);
 
 	RET2();
 
