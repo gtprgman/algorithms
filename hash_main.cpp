7bfcd67d183b49baa9ea98ea9071b77f84d8cb18
gtprgman <eddywu32@gmail.com>
2024-05-29 09:04:31 +0700

    RECENT CHANGES
    2024/MAY/29

--- hash_main.cpp
+++ hash_main.cpp
@@ -8,6 +8,7 @@
 
 
 
+
 int main(int argc, const char* argv[]) 
 {
 	std::string s = "She Sells Shells in the Sea Shore.";
@@ -22,6 +23,35 @@ int main(int argc, const char* argv[])
 
 	RET2();
 
+	hs.Sweep(); // Delete all elements
+	
+	for (const int i : s)
+		RPRINT((char)hs.get(i)._v);
+		
+	
+	RET;
+	
+	// define new data elements, the size previously determined in '_SZ' is not changed.
+	
+	hs = cElem<NODE_T>('H');
+	hs = cElem<NODE_T>('e');
+	hs = cElem<NODE_T>('l');
+	hs = cElem<NODE_T>('l');
+	hs = cElem<NODE_T>('o');
+
+	hs = cElem<NODE_T>('Y');
+	hs = cElem<NODE_T>('u');
+
+
+	RPRINT((char)hs.get('H')._v);
+	RPRINT((char)hs.get('i')._v);
+	RPRINT((char)hs.get('Y')._v);
+	RPRINT((char)hs.get('u')._v);
+	RPRINT((char)hs.get('l')._v);
+
+
+	
+	RET2();
+
 	return -1;
 }
-
