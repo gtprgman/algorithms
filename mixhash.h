24dba189612f839bffd54df666af55bf9b1c2a0b
gtprgman <eddywu32@gmail.com>
2024-06-15 11:19:34 +0700

    RECENT UPDATE
    The current update allowed cHash to evaluate a hash on Pointer to any types.

--- mixhash.h
+++ mixhash.h
@@ -1,11 +1,14 @@
 /* Using License: GPL v. 3.0 
-
-   This work is partly inspired from the 
-   material taken from 'A Practical Introduction to
-   Data Structre & Algorithm Analysis 3rd Edition (C++ Version)' 
-   (ebook) by Clifford A. Shaffer.
-
+	
+	This work is partly inspired from
+	the materials taken from the ebook
+	" A Practical Introduction to
+	Data Structures & Algorithm Analysis "
+	    3rd Edition ( C++ Version )
+		          by
+		   Clifford A. Shaffer
 */
+
 #pragma once
 
 #ifdef REQUIRE_H
@@ -42,16 +45,33 @@ inline const std::size_t permute(const std::size_t _TMinusOne)
 template <class T>
 struct cElem
 {
-	cElem() : KeyC(0), DataC(0), _head{ nullptr}, _next{ nullptr } {}
+	// AType could be a T or T*
+	using AType = typename type_aspect_if<T, mix::ptrTraits<T>::isPointer>::type;
 
-	cElem(T&& _instance) : KeyC(0), DataC(std::forward<T>(_instance))
+	// The T is made sure to be of its original type
+	using CType = typename type_aspect_if<T, false>::type;
+
+	// The T is determined to be of as what it was
+	using PType = typename mix::ptrTraits<T>::type;
+	
+	
+	cElem() : KeyC(0), DataC(AType()), _head{ nullptr}, _next{ nullptr }
+	{}
+
+	cElem(AType&& _instance) : KeyC(0)
 	{
+		if (mix::ptrTraits<AType>::isPointer)
+			DataC = _instance;
+		else
+			DataC = std::forward<AType>(_instance);
+
 		_head = nullptr;
 		_next = nullptr;
 	}
 
-	cElem(const T& _instance) : KeyC(0), DataC(_instance)
+	cElem(const AType& _instance) : KeyC(0)
 	{
+		DataC = _instance;
 		_head = nullptr;
 		_next = nullptr;
 	}
@@ -64,7 +84,7 @@ struct cElem
 
 
 	// move ctor
-	cElem(cElem<T>&& _iElem)
+	cElem(cElem<AType>&& _iElem)
 	{
 		if (this == &_iElem) return;
 		*this = std::move(_iElem);
@@ -72,7 +92,7 @@ struct cElem
 
 
 	// Move assignment
-	cElem<T>&& operator= (cElem<T>&& _mElem)
+	cElem<AType>&& operator= (cElem<AType>&& _mElem)
 	{
 		if (this == &_mElem)
 			return std::move(*this);
@@ -83,32 +103,47 @@ struct cElem
 		this->_head = std::move(_mElem._head);
 		this->_next = std::move(_mElem._next);
 
+		if (std::is_pointer_v<AType>) _mElem.DataC = nullptr;
+		else _mElem.DataC = AType();
 
 		_mElem.~cElem();
 		return std::move(*this);
 	}
 
-
-	T DataC;
+	
+	
+	AType DataC; // AType could be a pointer or a native std type
 	LongRange KeyC;
 
-	uniqueP<cElem<T>> _head;
-	uniqueP<cElem<T>> _next;
+	uniqueP<cElem<AType>> _head;
+	uniqueP<cElem<AType>> _next;
 
 	operator bool() const
 	{
-		return (DataC() != T());
+		if (mix::ptrTraits<AType>::isPointer)
+			return ( DataC != nullptr );
+		
+		return ( DataC != AType() );
 	}
 
 	const bool Empty() const
 	{
-		return (DataC() == T());
+		if (mix::ptrTraits<AType>::isPointer) return (DataC == nullptr);
+		return ( DataC() == AType());
 	}
 
 	
-	cElem(const cElem<T>&) = delete;
-	const cElem<T>& operator= (const cElem<T>&) = delete;
+	// Get the intrinsic data value of Ty
+	const CType Value() const
+	{
+		return (CType)DataC;
+	}
+
+
+	cElem(const cElem<AType>&) = delete;
+	const cElem<AType>& operator= (const cElem<AType>&) = delete;
 };
+
 
 
 
@@ -116,37 +151,40 @@ template <class T>
 class cHash;
 
 
-
 template < class Ty > /* partial template specialization for cHash<T>, 
   where T = cElem<Ty>; Ty depends on user supplied type */
+
 
 class cHash<cElem<Ty>>
 {
 public:
-	cHash() : _elemPtr(nullptr), _pList{nullptr} {}
-	cHash(const std::size_t _sz) : _elemPtr(nullptr), _M(_sz)
+
+	cHash() : isPtrC(0), _elemPtr(nullptr), _pList{nullptr} {}
+	cHash(const LongRange _sz) : isPtrC(0), _elemPtr(nullptr), _M(_sz)
 	{
 		_pList = MK_U_ARRAY<cElem<Ty>>((UINT)_sz);
+		isPtrC = mix::ptrTraits<Ty>::isPointer;
 	}
 
 
-	const std::size_t size() const { return _M;  }
+	const LongRange size() const { return _M;  }
+
 
 	// Returns a calculated hash value
-	const std::size_t operator()(const LongRange _K) const
+	const LongRange operator()(const LongRange _K) const
 	{
 		return hash(_K);
 	}
 
 
 	// Returns a recalculated hash by means of linear probing
-	const std::size_t operator()(const LongRange _K, const LongRange _c) const {
+	const LongRange operator()(const LongRange _K, const LongRange _c) const {
 		return probe(_K, _c);
 	}
 
 	 
 	// get the element at '_i' position of the internal array
-	const cElem<Ty>& elements(const std::size_t _i) const
+	const cElem<Ty>& elements(const LongRange _i) const
 	{
 		return _pList[_i];
 	}
@@ -158,14 +196,15 @@ public:
 	*/
 	const Ty get(const LongRange _uKey)
 	{
-		Ty _DataObj;
+		Ty _DataObj = (mix::ptrTraits<Ty>::isPointer)? nullptr : Ty();
 
-		std::size_t nPos = hash(_uKey);
+		LongRange nPos = (*this)(_uKey); // invoke hash method
 		
 		if (elements(nPos).KeyC != _uKey)
 		{
 			nPos = probe(nPos, 1);
 			_elemPtr = &_pList[nPos];
+
 			while (_elemPtr != nullptr)
 			{
 				if (_elemPtr->KeyC == _uKey)
@@ -190,14 +229,17 @@ public:
 
 	void operator= (cElem<Ty>&& _rElem)
 	{
-		const LongRange _key = _rElem.DataC();
-		std::size_t nPos = hash(_key);
+		Ty _key;
+
+		_key = _rElem.DataC;
+
+		LongRange nPos = hash( (LongRange)_key );
 
 		if (nPos > _M) --nPos;
 
 		// if the returned slot position hasn't been occupied by other..
 		if (!Exist(nPos)) {
-			_rElem.KeyC = _key;
+			_rElem.KeyC = (LongRange)_key;
 			_pList[nPos] = std::move(_rElem);
 			return;
 		}
@@ -208,13 +250,13 @@ public:
 			nPos = probe(nPos, 1); // searches other possible empty slot..
 			if (!Exist(nPos) ) // if any found slot has not been occupied..
 			{
-				_rElem.KeyC = _key;
+				_rElem.KeyC = (LongRange)_key;
 				_pList[nPos] = std::move(_rElem);
 				return;
 			}
 			else
 			{
-				_rElem.KeyC = _key;
+				_rElem.KeyC = (LongRange)_key;
 				_elemPtr = (cElem<Ty>*)add_next_links(&_pList[nPos]);
 
 				if ( _elemPtr != nullptr )
@@ -226,17 +268,17 @@ public:
 	}
 
 	
-	const cElem<Ty>& Exist(const std::size_t _hPos) const
+	const cElem<Ty>& Exist(const LongRange _hPos) const
 	{
 		return _pList[_hPos];
 	}
 
 
 
-	// erases all the data elements in the array
+	// Erases all the data elements in the array
 	void Sweep() const
 	{
-		for (std::size_t i = 0; i < _M; i++)
+		for (LongRange i = 0; i < _M; i++)
 			_pList[i].~cElem();
 	}
 
@@ -244,24 +286,25 @@ public:
 
 	
 private:
-	std::size_t _M;
+	LongRange _M;
 
 	// a raw pointer to each linked element of _pList
 	cElem<Ty>* _elemPtr;
 
 	U_ARRAY<cElem<Ty>> _pList;
 
-	
+	bool isPtrC;
 
-	const std::size_t hash(const LongRange& _Key) const
+	
+	const LongRange hash(const LongRange& _Key) const
 	{
-		return _Key % (LongRange)_M;
+		return ( _Key % _M  );
 	}
 
 
-	const std::size_t probe(const LongRange& _Key, const LongRange& _ci) const
+	const LongRange probe(const LongRange& _Key, const LongRange& _ci) const
 	{
-		LongRange _home = (LongRange)hash(_Key);
+		LongRange _home = hash(_Key);
 		return (_home + _ci) % _M;
 	}
 
@@ -286,8 +329,7 @@ private:
 
 		return (_elemPtr->_next.get());
 	}
-
-	
 };
+
 
 #endif
