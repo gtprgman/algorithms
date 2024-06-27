5f403e7612221d44bd318f3d310c47d893f5789d
gtprgman <eddywu32@gmail.com>
2024-06-27 10:48:51 +0700

    RECENT UPDATE
    The current update enables the evaluation of a negative decimal integer to a particular assembly of bits. Added a new member method called 'to_signed_bits()'.

--- mixbit.h
+++ mixbit.h
@@ -6,6 +6,88 @@
 
 #endif
 
+constexpr unsigned BYTE = 8;
+constexpr unsigned WORD = 16;
+constexpr unsigned DWORD = 32;
+constexpr unsigned DDWORD = 64;
+
+
+// the max. number of bits evaluated by 'BIT_TOKEN()'
+unsigned MAX_BIT = 0;
+
+
+// returns a specific named token which evaluates to a max. number of bits.
+constexpr unsigned BIT_TOKEN(const unsigned nBits)
+{
+	if (nBits <= BYTE) MAX_BIT = BYTE;
+	else if (nBits > BYTE && nBits <= WORD) MAX_BIT = WORD;
+	else if (nBits > WORD && nBits <= DWORD) MAX_BIT = DWORD;
+	else MAX_BIT = DDWORD;
+
+	return MAX_BIT;
+}
+
+
+// invert every bit in the bit array.
+void invert_bits(bool _pb[], const unsigned nBits)
+{
+	for (unsigned j = 0; j < nBits; j++)
+	{
+		_pb[j] = !_pb[j];
+	}
+}
+
+
+
+
+// get a bit character from a position in the bit array as specified by '_pb'.
+const char* char_from_bit(const bool _pb[], const int _index)
+{
+	return (_pb[_index])? (char*)"1" : (char*)"0";
+}
+
+// get a value from a position in the bit array 
+const unsigned n_of_bit(const bool _pb[], const int _index)
+{
+	return _pb[_index];
+}
+
+
+// obtain a full bits string from the bit array specified by '_pb'.
+const std::string str_from_bits(const bool _pb[], const unsigned nBits)
+{
+	char* _ss = new char[nBits];
+
+	for (unsigned i = 0; i < nBits; i++)
+		_ss[i] =  (_pb[i])? 49 : 48;
+	
+	_ss[nBits] = '\0';
+	return _ss;
+}
+
+
+/* obtain a bits from string data, this includes the most significant bits padded
+   to the left of primary bits */
+const bool* bits_from_str(const std::string& _cBits)
+{
+	
+	const unsigned numBits = (unsigned)std::strlen(_cBits.data());
+	unsigned nZeros = BIT_TOKEN(numBits) - numBits;
+
+	bool* _pb = new bool[nZeros + numBits];
+
+	// pad trailing bits with '0'
+	for (unsigned z = 0; z < nZeros; z++) _pb[z] = false;
+
+	// top-up leading bits with '_cBits'
+	for (unsigned i = 0; i < numBits; i++)
+	{
+		_pb[nZeros++] = (_cBits.c_str()[i] == 49)? true : false;
+	}
+
+	return _pb;
+}
+
 
 
 struct BitN
@@ -106,6 +188,53 @@ struct BitN
 		return _v;
 	}
 
+
+	// obtains a 2-complement bits string from a specific signed value ( + / - )
+	const std::string& to_signed_bits(const int _signed_v)
+	{
+		_bitStr = "\0";
+		bool* _bits = nullptr;
+		unsigned _v = 0;
+
+		unsigned _dec = (_signed_v < 0)? std::abs(_signed_v) : _signed_v;
+		_bitStr = toBits(_dec);
+		MAX_BIT = BIT_TOKEN((unsigned)std::strlen(_bitStr.data()));
+
+		// if a negative decimal value
+		if (_signed_v < 0)
+		{
+			_bits = (bool*)bits_from_str(_bitStr);
+			
+			_bitStr = "\0";
+			
+			invert_bits(_bits, MAX_BIT);
+
+			// get the inverted bits string
+			_bitStr = str_from_bits(_bits, MAX_BIT);
+
+			// convert the inverted bits string into a value
+			_v = value_from_bitstr(_bitStr);
+
+			++_v; // added 1 to the inverted bits
+
+			// convert the value into bits string again
+			_bitStr = "\0";
+			_bitStr = toBits(_v);
+
+			// convert the bits string into a bit array
+			delete[] _bits; // clear previous allocated bits
+			_bits = (bool*)bits_from_str(_bitStr);
+
+			// convert back the bit array into bits string
+			_bitStr = "\0";
+			_bitStr = str_from_bits(_bits, MAX_BIT);
+			_bitLen = std::strlen(_bitStr.c_str());
+			
+		}
+
+		delete[] _bits;
+		return _bitStr;
+	}
 
 	const size_t bitSize() const { return _bitLen; }
 
