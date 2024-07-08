4c3e1f4aa0e7c9d695576f914f6338aa84580fdc
gtprgman <eddywu32@gmail.com>
2024-07-08 10:39:50 +0700

    RECENT CHANGES
    2024/JUL/08: Demo Fixed Point Type in mixbit.h

--- bitops.cpp
+++ bitops.cpp
@@ -8,29 +8,33 @@
 
 int main()
 {
-	BitN bitFun;
-	
-	std::string f1 = "3.8";
-	bitFun.to_fixed_point_bits(f1);
-	bitFun(); 
-	
-	RET2();
+	std::cout.precision(8);
 
-	f1 = "8.75";
-	bitFun.to_fixed_point_bits(f1);
-	bitFun(); 
-	
-	RET2();
+	fixN<32> f1 = 7.0221;
+	fixN<32> f2 = 3.0051;
 
-	f1 = "8.63";
-	bitFun.to_fixed_point_bits(f1);
-	bitFun(); 
-	
-	RET2();
 
-	f1 = "1.625";
-	bitFun.to_fixed_point_bits(f1);
-	bitFun(); 
+	PRINT(f1()); RET;
+	PRINT(f2()); RET;
+
+	RPRINT(f1() + f2()); RET;
+	
+	RPRINT(f1() * f2()); RET;
+
+	RET;
+	PRINT(f1.rational()); RET;
+	PRINT(f2.rational()); RET;
+
+	RET;
+
+	PRINT(f1.decimal()); RET;
+	PRINT(f2.decimal()); RET;
+
+
+	fixN<16> d = 33570; 
+
+	PRINT(d(0)); RET;
+
 	
 
 	RET2();
