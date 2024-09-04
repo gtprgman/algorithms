d7a1f66e17de4604aa616205e0b2c27488920b6f
gtprgman <eddywu32@gmail.com>
2024-09-04 08:52:15 +0700

    RECENT CHANGES
    2024/SEP/04 : 
    - Demo encoding processes.
    - Demo bit packing the resulted huffman bits pattern.

--- TEST_HUFF_READY_03.cpp
+++ TEST_HUFF_READY_03.cpp
@@ -9,16 +9,17 @@
 
 int main() {
 	
-	std::string s = "She Sells Shells in the Sea Shore.";
+	std::string s = "Ada Ate Apple.";
 	const std::size_t SZ = s.size();
 	std::vector<node> nod, fNod;
+	std::vector<bitInfo<unsigned int> > bfo;
 	std::map<int, char> mPair;
 	
 
 	for (const auto& c : s)
 		nod.push_back(c);
 
-
+	
 	sort_Nodes<int>(nod, SZ);
 
 	filter_Nodes(fNod, nod); 
@@ -27,31 +28,53 @@ int main() {
 
 	sort_Nodes<double>(fNod, fNod.size());
 
-	//NPRINT(fNod);
+	//NPRINT(fNod); RET;
 
 	build_huffman_tree(fNod);
 
-	_TREE::Info(); RET;
+	/*
+	 Debugging purposes, this funct should be execute independently
+	 without precede or after call to 'build_huffman_code()' 
+	 
+	*/
+	//nodes_tree_hierarchy(_TREE::_Root);
+
+	RET;
+	
 
 	build_huffman_code(mPair, _TREE::_Root);
 
 	RET;
 
-
 	for (const std::pair<int, char>& mp : mPair)
 	{
-		RPRINT(mp.second); RPRINT("->"); RPRINT(biXs.toBits(mp.first).data());
+		//RPRINT(mp.second); RPRINT("->"); RPRINT(biXs.toBits(mp.first).data() );
+		bfo.push_back({ mp.first,num_of_bits<unsigned int>::eval(mp.first) + 1 });
+
 		RET;
 	}
-
-	fNod.clear(); 
-	mPair.clear();
 
 
 	RET;
 	
+	
+	const UINT bfPacked = bitsPack<UINT>(bfo);
+
+	RPRINT("Packed Integer Value: "); RPRINT(bfPacked); RET2();
+
+	RPRINT("Packed Size: ");  RPRINT(sizeof(bfPacked)); RET2();
+
+	PRINT(to_binary<UINT>::eval(bfPacked));
+
+	RET;
 
 	RET2();
+
+
+	fNod.clear();
+	mPair.clear();
+	bfo.clear();
+
 	return -1;
 }
 
