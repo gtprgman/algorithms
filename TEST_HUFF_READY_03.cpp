12cd8c859e31283bae405a394edd1586c5e1e822
gtprgman <eddywu32@gmail.com>
2024-10-12 10:41:45 +0700

    RECENT STABLE 2(i)
    2024/OCT/12:
    Reflect the current update in mixhuff.h and mixhuff_impls.h

--- TEST_HUFF_READY_03.cpp
+++ TEST_HUFF_READY_03.cpp
@@ -5,28 +5,40 @@
 #include "mixhuff.h"
 
 
+/*
+	The current stable version is Pointers-Free !!
+	You can now run the program in Debugging Mode, without a fear of crash.
+	Just press [F5] to run..
+*/
+
 
 int main()
 {
-	std::string s = "Ada Ate Apple.";
+	std::string s = "Hello World You.";
 	const std::size_t SZ = s.size();
 	std::priority_queue<node> pq;
 	std::vector<node> fuNod;
 	std::vector<bitInfo<UINT>> bfo;
 	std::vector<UINT> packed;
-	std::map<int, char> codeMap;
+	std::vector<BPAIR> vCodeMap;
+	BPAIR _bpt = {};
 
+
+	// Collect raw data from input sources
 	for (const auto& e : s)
 		pq.push(e);
 
-
+	// filter the data and classifying them based on frequency of appearances
 	for (; !pq.empty(); )
 	{
-		filter_pq_nodes(fuNod, (NODE_T)pq.top(), pq.size());
+		filter_pq_nodes(fuNod, (node&&)pq.top(), pq.size());
 		pq.pop();
 	}
 
+	
+	RET;
 
+	// sort the data based on frequencies
 	std::priority_queue<node, std::vector<node>,
 		fqLess<node>> fpq{ fuNod.begin(), fuNod.end() };
 
@@ -34,22 +46,46 @@ int main()
 
 	fuNod.clear();
 
-	for (NODE_T _nf = 0; !fpq.empty(); )
+	// top up the vector from the frequencies-sorted data
+	for (node _nf = 0; !fpq.empty(); )
 	{
 		_nf = fpq.top();
 		fuNod.push_back(_nf);
 		fpq.pop();
 	}
 
-	_TREE::plot_huffman_tree(fuNod);
+	
+	_TREE::plot_tree(fuNod);
+
+	vCodeMap = _TREE::CodeMap();
+
+	// display the encoding table
+	for (std::vector<BPAIR>::iterator _Vit = vCodeMap.begin(); _Vit != vCodeMap.end(); _Vit++)
+	{
+		//RPRINT(_Vit->_data); RPRINT("->"); RPRINT(to_binary<int>::eval(_Vit->_val).data());
+		bfo.push_back({ _Vit->_val,
+					  oneAdder(num_of_bits<UINT>::eval(_Vit->_val)) });
+		RET;
+	}
+
+	bitsPack(packed, bfo);
+
+	// display the packed encoding data
+	for (const UINT ui : packed)
+	{
+		PRINT(to_binary<UINT>::eval(ui).data());;
+	}
+
 	
 	fuNod.clear();
-	codeMap.clear();
-
+	vCodeMap.clear();
+	
 	bfo.clear();
 	packed.clear();
-
+	
+	_TREE::Clean();
 
 	RET2();
 	return -1;
+
 }
