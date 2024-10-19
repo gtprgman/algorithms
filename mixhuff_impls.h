3af3bcb2e0fc9bc664bf16ccafd0eb3b255865fe
gtprgman <eddywu32@gmail.com>
2024-10-19 10:31:17 +0700

    RECENT UPDATE
    2024/OCT/19:
    Updated '_TREE::create_encoding()'
    This update enforces unique the overall bits pattern generated by the encoding mechanism.

--- mixhuff_impls.h
+++ mixhuff_impls.h
@@ -3,13 +3,13 @@
 
 #ifndef MX_HUFF_IMPLS
 	#define MX_HUFF_IMPLS
-
+	#include <fstream>
 #endif
 
 
 
 inline static void filter_pq_nodes(std::vector<node>& _target, node&& _Nod,
-									const std::size_t _maxLen)
+				    const std::size_t _maxLen)
 {
 	node _nod = _Nod; //fetch new node from priority queue every time this function is called
 	double _fqr = 0;
@@ -64,24 +64,24 @@ inline void _TREE::create_encoding(const int _From,
 
 		else if (_fq == _e.FrequencyData())
 		{
-			_recurr++;
 			_Dir = R;
-
-			if (_recurr > 1)
-			{
-				_sameVal = biXs.value_from_bitstr(_bt.data());
-				++_sameVal; _bt.clear();
-				_bt.assign(biXs.toBits(_sameVal).data());
-				_bt = LRTrim(_bt.data());
-				_vPair.push_back({ _e.dataValue(),biXs.value_from_bitstr(LRTrim(_bt.data())) });
-				_fq = _e.FrequencyData();
-				continue;
-			}
+			_recurr++;
 		}
+	  
 		_fq = _e.FrequencyData();
 
+		if (_recurr > 1)
+		{
+			_sameVal = biXs.value_from_bitstr(_bt.data());
+			++_sameVal;
+			_bt.clear();
+			_bt.assign(to_binary<UINT>::eval(_sameVal).data());
+			_recurr = 0;
+		}
+
 		_bt = (char*)concat_str((char*)_bt.data(), inttostr(_Dir));
-		_vPair.push_back({ _e.dataValue(),biXs.value_from_bitstr(LRTrim(_bt.data())) });
+
+		_vPair.push_back({ _e.dataValue(),(UINT)biXs.value_from_bitstr(LRTrim(_bt.data())) });
 	}
 }
 
@@ -96,8 +96,59 @@ inline void _TREE::schema_Iter(const std::vector<node>& _fpNods)
 	if (!_vPair.empty()) _vPair.clear();
 	create_encoding(0, _halfTreeSz, _bts, _fpNods);
 
-	_bts = "1";
+	_bts = "10";
 	create_encoding(_halfTreeSz, _TreeSizes, _bts, _fpNods);
 
+}
+
+
+inline static const std::size_t writePack(const std::string& _fiName, const std::vector<UINT>& _pacData)
+{
+	std::size_t _numWritten = 0;
+	const std::size_t _packSz = _pacData.size();
+	std::ofstream _out{ _fiName.data(), std::ios::out | std::ios::ate | std::ios::binary };
+
+
+	if (!_out) {
+		std::cerr << "Failed to open file !!" << "\n\n";
+		_out.close();
+		return _numWritten;
+	}
+
+	for (const UINT& _ui : _pacData)
+	{
+		_out.put(_ui);
+		++_numWritten;
+	}
+
+	_out.close();
+
+	return _numWritten;
+}
+
+
+inline static const std::size_t readPack(const std::string& _inFile)
+{
+	std::size_t _totReads = 0;
+	std::ifstream _inf{ _inFile.data(),std::ios::in | std::ios::binary };
+	int _C = 0;
+
+	if (!_inf)
+	{
+		std::cerr << "Can't open file to read !!" << "\n\n";
+		_inf.close();
+		return _totReads;
+	}
+
+	while( (_C = _inf.get()) > -1 )
+	{
+		_totReads++; 
+		RPRINT(_C);
+	
+	}
+
+	_inf.close();
+
+	return _totReads;
 }
 
