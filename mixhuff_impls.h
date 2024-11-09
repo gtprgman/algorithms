93e66cf93c03b9879e639ef868e3a9cbd9c4222f
gtprgman <eddywu32@gmail.com>
2024-11-09 11:03:14 +0700

    RECENT UPDATE
    2024/NOV/09 :
    Added new function : ReSync()
    to resync the read bit versus the original packed one.

--- mixhuff_impls.h
+++ mixhuff_impls.h
@@ -7,12 +7,54 @@
 #endif
 
 
+// ReSync the read bits versus the original packed one
+inline static void ReSync(std::vector<BPAIR>& _readVec, const std::vector<int>& _Packed)
+{
+	const std::size_t _maxSz = _Packed.size();
+	std::size_t j = 0;
+	int _hi = 0, _mid = 0, _lo = 0, _Single = 0;
+
+
+	for (std::size_t g = 0; g < _maxSz; g++)
+	{
+		j = g;
+		while (_readVec[j]._val < 0) ++j;
+
+		if ( _Packed[g] != _readVec[g]._val )
+		{	
+			j = g;
+			MAX_BIT = proper_bits(_Packed[j] );
+			_hi = _readVec[j]._val; 
+			_lo = _readVec[j + 1]._val;
+
+				if (MAX_BIT == WORD) {
+					_hi <<= 8; // Shifts _hi to the WORD's MSB
+					_Single = _hi | _lo;
+					_readVec[j]._val = _Single;
+					_readVec[j + 1]._val = -1;
+				}
+
+				else if (MAX_BIT == DWORD) {
+					_hi <<= 16;
+					_mid = _lo << 8;
+					_lo = _readVec[j + 2]._val;
+
+					_Single = (_hi | _mid | _lo);
+					_readVec[j]._val = _Single;
+					_readVec[j + 1]._val = -1;
+					_readVec[j + 2]._val = -1;
+				}
+		}
+		continue;
+	}
+}
+
 
 inline static void filter_pq_nodes(std::vector<node>& _target, node&& _Nod,
-									const std::size_t _maxLen)
+				  const std::size_t _maxLen)
 {
-	node _nod = _Nod; /* fetches new node from the priority queue each time
-			     this function is called. */
+	node _nod = _Nod; /*  fetches new node from the priority queue each time
+			      this function is called. */
 	double _fqr = 0;
 	static int _q = 0;
 	int _p = _q;
@@ -82,7 +124,7 @@ inline void _TREE::create_encoding(const int _From,
 
 		_bt = (char*)concat_str((char*)_bt.data(), inttostr(_Dir));
 
-		_vPair.push_back({ _e.dataValue(),(UINT)biXs.value_from_bitstr(LRTrim(_bt.data())) });
+		_vPair.push_back({ _e.dataValue(),biXs.value_from_bitstr(LRTrim(_bt.data())) });
 	}
 }
 
@@ -101,6 +143,8 @@ inline void _TREE::schema_Iter(const std::vector<node>& _fpNods)
 	create_encoding(_halfTreeSz, _TreeSizes, _bts, _fpNods);
 
 }
+
+
 
 
 inline static const std::size_t writePack(const std::string& _fiName, const std::vector<int>& _pacData)
@@ -143,11 +187,13 @@ inline static const std::size_t writePack(const std::string& _fiName, const std:
 }
 
 
-inline static const std::size_t readPack(const std::string& _inFile)
+inline static const std::size_t readPack(const std::string& _inFile,
+					std::vector<BPAIR>& _ReadVector)
 {
 	std::size_t _totReads = 0;
 	std::ifstream _inf{ _inFile.data(),std::ios::in | std::ios::binary };
 	int _C = 0;
+	BPAIR _Rec = { 0,0 };
 
 	if (!_inf)
 	{
@@ -159,8 +205,11 @@ inline static const std::size_t readPack(const std::string& _inFile)
 	while( _C >= 0 )
 	{
 		_C = _inf.get();
+		_Rec._val = _C;
+		_ReadVector.push_back(_Rec);
 		_totReads++; 
-		RPRINT(_C);
+		_Rec = {0,0};
+
 	}
 
 	_inf.close();
