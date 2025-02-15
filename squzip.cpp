ee67f46f6706ffb8ce238fff78398accc756b29e
gtprgman <eddywu32@gmail.com>
2025-02-15 10:29:39 +0700

    LATEST UPDATE
    2025 / FEB / 15 :
    Latest Update.

--- squzip.cpp
+++ squzip.cpp
@@ -1,3 +1,5 @@
+/ * Using License: GPL v.3.0  */
+	
 #include <filesystem>
 
 #ifndef REQUIRE_H
@@ -6,15 +8,16 @@
 #endif
 
 
+
 /*
-; Usages: 	  For compressing one file into a *.sqz file.
-		  squzip -q <file1.[ext]> <file2.sqz> <COMP_RATE>
-		  eg: " squzip -q Letter1.txt Letter1.sqz 0 " --> uses default COMP_RATE
-		      " squzip -q Letter1.txt Letter1.sqz 0.65 " --> takes COMP_RATE specified by user.
+; Usages: For compressing one file into a *.sqz file.
+		  squzip -q <file1.(ext)> <file2.sqz> [COMP_RATE]
+		  eg: "squzip -q Letter1.txt Letter1.sqz" --> uses default COMP_RATE
+		      "squzip -q Letter1.txt Letter1.sqz 0.65" --> takes COMP_RATE specified by user.
 
 		  For uncompressing a *.sqz into its original format
-		  squzip -d <file2.sqz> <fileX.[ext]> <0>
-		  eg: " squzip -d Letter1.sqz Letter1A.txt 0 "
+		  squzip -d <file2.sqz> <fileX.(ext)> [0]
+		  eg: "squzip -d Letter1.sqz Letter1A.txt"
 
 */
 
@@ -25,37 +28,41 @@ static char* _pSource = nullptr;
 
 int main(const int argc, const char* args[MAX])
 {
-	/*             0                 1                   2            3             4
-	  args ['program_name'] [-q /-d / -s /-t / -o ] [input_file] [output_file] [ COMP_RATE ]  
+	/*             0            1          2            3             4
+	  args ['program_name'] [-q /-d ] [input_file] [output_file] [ COMP_RATE ]  
 	  
-	  NB: A minimum of 3 input arguments must be specified to the squzip command.
+	  NB: A minimum of 4 input arguments must be specified to the squzip command.
     */
-
 	
-	const char* _c0 = (args[1])? args[1] : "\0";
-	
-	const double _d1 = (args[4])? std::strtod(args[4], nullptr) : 0.00;
+		const char* _c0 = (args[1])? args[1] : "\0";
 
-	std::string _f0 = (args[2])? args[2] : "\0",  // raw input file
-		    _f1 = (args[3])? args[3] : "\0";  // target output file
+		std::string _f0 = (args[2])? args[2] : "\0",  // raw input file
+			    _f1 = (args[3])? args[3] : "\0",  // target output file
+			    _fN = (args[4])? args[4] : "\0"; // COMP_RATE argument
+
+		const double _d1 = (_fN.empty())? 0 : std::strtod(_fN.data(), nullptr);
 
 
-	switch (_c0[1])
-	{
-		case 'q' :
-			Compress(_f1,_f0, _d1 ,_pSource);
-			break;
+		switch (_c0[1])
+		{
+		case 'q':
+			Compress(_f1, _f0, _d1, _pSource);
+			goto EndStop;
 
-		case 'd' :
+		case 'd':
 			UnCompress(_f0, _f1);
-			break;
+			goto EndStop;
 
 		default:
 			PRINT("unknown command or bad filename.");
-			break;
-	}
+			goto EndStop;
+		}
+	
+
+EndStop:
 
 	return -1;
 }
+
 
 
