cd09fb8402dbe197b91d0c2f7ec9b11193137aa7
gtprgman <eddywu32@gmail.com>
2025-12-25 10:12:28 +0700

    CURRENT UPDATE
    2025/DEC/25: UPDATE REFLECT TO THE LATEST CHANGES

--- fTest9.c
+++ fTest9.c
@@ -11,7 +11,7 @@ int main()
 	std::vector<_Canonical> vci = {};
 	std::vector<node> vNodes = {};
 
-	_Canonical cn = {};
+	Can_Bit cn = {};
 
 	/* for example, this is the generated huffman encoding information */
 	std::vector<_Canonical> vcn = {
@@ -23,7 +23,7 @@ int main()
 	};
 
 	mix::generic::fast_sort(vcn.begin(), vcn.end(), mix::generic::NLess<char>());
-	mix::generic::fast_sort(vcn.begin(), vcn.end(), mix::generic::NLess<int>());
+	mix::generic::fast_sort(vcn.begin(), vcn.end(), mix::generic::NLess<intmax_t>());
 
 
 	_Gen_Canonical_Info(vci, vcn);
@@ -32,55 +32,66 @@ int main()
 
 	/*
 	for (const auto& _ce : vci) {
-		RPRINTC(_ce._xData); RPRINTC(_ce._codeWord); RET;
+		RPRINTC(_ce._xData); RPRINTC(_ce._bitLen); RET;
 	}
 	*/
+	
 	RET;
 
 	vectorClean(vci);
 	vectorClean(vcn);
 
+
 	/*
 		 THIS PART OF CODES SHOW THE PROCESSES INVOLVED IN COMPRESSING A GENERATED HUFFMAN CODES
 		 WHICH GENERALLY WE HAD REFERED TO IT AS 'ENCODING HEADER INFORMATION' THAT WOULD BE SAVED
 		 TO THE BEGINNING OF '*.sqz ' FILE JUST BEFORE THE CORE COMPRESSED DATA WOULD BE WRITTEN IN.
-	
+
 	*/
 
 	std::vector<UC> _EncDat = { 'A','B','C','D','R' };
 	intmax_t _EncInt = 0;
 	std::vector<BPAIR> _EncBP;
-	std::vector<int64_t> _EncPac;
+	std::vector<intmax_t> _EncPac;
 	size_t _EncHeadInt = 0;
 	std::FILE* _fEncHead = nullptr;
 	std::vector<_Canonical> _Cnh1, _Cnh2;
 
+	std::string _fname = "C:\\PROJECTS\\SAMPLES\\header_f.sqz";
+
 	_EncInt = Gen_Encoding_Info(_EncDat, _EncBP, vci, _EncPac, COMP_RATE); RET;
-	/*
-	for (const auto& _e : _EncPac)
-	{
-		RPRINTC(_e);
-	}
-	*/
+	
 	RET;
-
-	_EncInt = cni_bits_pack(_EncPac); RET;
-
-	_EncHeadInt = Gen_Cni_Header_Info(_Cnh1, _Cnh2, vci, _EncPac);
 
 	
 	RET;
+
+	_EncHeadInt = Gen_Cni_Header_Info(_Cnh1,_Cnh2, vci);
+
+	/*
+	for (const auto& c1 : _Cnh1)
+	{
+		RPRINTC(c1._xData); RPRINTC(c1._bitLen); RPRINTC(c1._rle_bit_len); RET;
+	}
+	*/
+
+	RET;
+
+	_EncHeadInt = Write_Header(_fname.c_str(), _fEncHead, _Cnh1, _Cnh2, 'D');
+
 
 	vectorClean(vci);
 	vectorClean(_EncDat);
 	vectorClean(_EncBP);
 	vectorClean(_EncPac);
-	
+
 	vectorClean(_Cnh1);
 	vectorClean(_Cnh2);
 
 	if (_fEncHead) std::fclose(_fEncHead);
 
 	return 0;
-
 }
+
+
+	
